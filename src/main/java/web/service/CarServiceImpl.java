973c3aeed3d3d939eacb5ce825900d08d8828bd1
a-levyakov <a.levyakov95@gmail.com>
2025-03-25 19:30:28 +0300

    1

---
+++ src/main/java/web/service/CarServiceImpl.java
@@ -0,0 +1,31 @@
+package web.service;
+
+import org.springframework.stereotype.Service;
+import web.model.Car;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Service
+public class CarServiceImpl implements CarService {
+
+
+    @Override
+    public List<Car> getCarsFromList(int count) {
+        return getCars().stream().limit(count).collect(Collectors.toList());
+    }
+
+    public static List<Car> getCars() {
+        List<Car> result = new ArrayList<>();
+
+        result.add(new Car("Bwm", 2011, "black"));
+        result.add(new Car("Opel", 1993, "blue"));
+        result.add(new Car("Mazda", 2015, "red"));
+        result.add(new Car("Kia", 1995, "green"));
+        result.add(new Car("Lada", 1990, "orange"));
+
+        return result;
+    }
+}
